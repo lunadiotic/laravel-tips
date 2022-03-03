a17b4a767c215e747296032fa5d30c8ab2bb3076
IDStack <idstack@outlook.co.id>
2022-03-03 18:10:41 +0700

    cara baru render string

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 
@@ -20,6 +21,7 @@ Route::get('/', function () {
     // return Str::of('hey you')->append(' and everybody there');
     // return str('this is a title')->slug();
     // return str()->slug('this is a title');
+    return Blade::render('{{ $greeting }}, World', ['greeting' => 'Hi']);
     return view('welcome');
 })->name('home');
 
